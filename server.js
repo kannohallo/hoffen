ea148f7f39919bd3d17004b86e250ed8e33689c0
Glitch (hello-express) <none>
2020-10-24 22:10:51 +0000

    🏈📌 Checkpoint ./server.js:16809183/1050

--- server.js
+++ server.js
@@ -38,9 +38,51 @@ const config = require ("./config.json");
 const client = new discord.Client();
 const token = config.token
 const prefix = config.prefix
-const embed = new discord.MessageEmbed();
 var member = discord.user;
-client.login(token)
+client.login(token);
+
+client.on("preparado", () => {
+  const actividades = [
+    "en desarollo",
+    "gestionado por kanno",
+    "para reportar escribe h!own",
+    "mi prefix es h!"
+  ];
+  setInterval(() => {
+    const index = Math.floor(Math.random() * (actividades.length - 1) + 1);
+
+    client.user.setPresence({
+      game: {
+        name: actividades[index],
+        type: "PLAYING",
+        browser: "Discord iOs"
+      },
+      status: "online"
+    });
+  }, 5000);
+});
+
+//////////////////c/////////////////////////////
+client.on("message", msg =>{
+  const embed = discord.MessageEmbed()
+    .setTitle("Estos son mis comandos por el momento")
+
+
+
+
+
+  if (msg.content.startsWith (prefix + "help"))
+
+
+
+
+
+
+});
+
+
+
+
 
 
 

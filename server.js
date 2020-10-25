ef2a31c93f8d5460d993719beb736af6bcc3223e
Glitch (hello-express) <none>
2020-10-25 11:20:17 +0000

    🏫🛩 Checkpoint ./server.js:16809183/423 ./package.json:16809183/3155

--- server.js
+++ server.js
@@ -3,6 +3,15 @@
 
 
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kannohallo:<Innovando.2002>@hoffendatabase.hqous.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});const mo
+
 
 const marsnpm = require ("marsnpm")
 var cheerio = require("cheerio");
